f506098ef900ae0c9069e6727407fe43019cc999
songyu <289483936@qq.com>
2024-04-27 14:07:08 +0800

    okk

---
+++ assets/productList-DQpiE5Oj.js
@@ -0,0 +1,1 @@
+import{A as o}from"./request-CDtsqYAV.js";let a=t=>o.get("https://css2-uat.tata.com.cn/mall/goods/queryGoodsList",{params:t}),d=t=>o.post("https://css2-uat.tata.com.cn/mall/goods/editGoods",t),c=t=>o.get("https://css2-uat.tata.com.cn/mall/goods/startStopGoods",{params:t}),p=t=>o.post("https://css2-uat.tata.com.cn/mall/goods/addGoods",t);export{p as a,d as b,a as p,c as s};
